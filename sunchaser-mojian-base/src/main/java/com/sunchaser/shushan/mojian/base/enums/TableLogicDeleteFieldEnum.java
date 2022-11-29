b161b8d3811086f5f77a403c3dac9a5de0a9fa1f
lilu <admin@lilu.org.cn>
2022-11-30 00:32:03 +0800

    feat: update table status and logic-delete field enum

---
+++ sunchaser-mojian-base/src/main/java/com/sunchaser/shushan/mojian/base/enums/TableLogicDeleteFieldEnum.java
@@ -0,0 +1,21 @@
+package com.sunchaser.shushan.mojian.base.enums;
+
+/**
+ * 表逻辑删除字段 is_deleted 枚举
+ *
+ * @author sunchaser admin@lilu.org.cn
+ * @since JDK8 2021/7/17
+ */
+public enum TableLogicDeleteFieldEnum {
+
+    /**
+     * normal
+     */
+    NORMAL,
+
+    /**
+     * deleted
+     */
+    DELETED,
+    ;
+}
---
+++ sunchaser-mojian-base/src/main/java/com/sunchaser/shushan/mojian/base/enums/TableStatusFieldEnum.java
@@ -0,0 +1,20 @@
+package com.sunchaser.shushan.mojian.base.enums;
+
+/**
+ * 表 status 字段枚举
+ *
+ * @author sunchaser admin@lilu.org.cn
+ * @since JDK8 2022/11/2
+ */
+public enum TableStatusFieldEnum {
+
+    /**
+     * 正常
+     */
+    NORMAL,
+
+    /**
+     * 停用、禁用
+     */
+    FORBIDDEN;
+}
