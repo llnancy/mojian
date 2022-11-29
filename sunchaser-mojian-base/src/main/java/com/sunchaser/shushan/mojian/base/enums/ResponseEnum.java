04cb3d58e892d27ad928b2339ae36c0eb014f5a5
lilu <admin@lilu.org.cn>
2022-11-30 00:32:21 +0800

    feat: update ResponseEnum.java

--- sunchaser-mojian-base/src/main/java/com/sunchaser/shushan/mojian/base/enums/ResponseEnum.java
+++ sunchaser-mojian-base/src/main/java/com/sunchaser/shushan/mojian/base/enums/ResponseEnum.java
@@ -18,10 +18,13 @@ public enum ResponseEnum implements Response {
      * 内置状态码
      */
     SUCCESS(20000, "请求成功"),
+    LOGOUT_SUCCESS(20002, "退出成功"),
     INVALID_PARAM(40000, "参数有误"),
     UNAUTHORIZED(40100, "未经授权"),
     FORBIDDEN(40300, "暂无权限"),
-    FAILURE(50000, "请求失败");
+    FAILURE(50000, "请求失败"),
+    BAD_CREDENTIALS(50001, "用户名或密码错误"),
+    ;
 
     private final Integer code;
 
