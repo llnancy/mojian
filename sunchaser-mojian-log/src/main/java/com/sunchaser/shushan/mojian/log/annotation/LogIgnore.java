97d550e0b7f4501bf02d860142e9c23bf66686a4
lilu <admin@lilu.org.cn>
2022-12-01 00:08:03 +0800

    feat: add LogIgnore.java

---
+++ sunchaser-mojian-log/src/main/java/com/sunchaser/shushan/mojian/log/annotation/LogIgnore.java
@@ -0,0 +1,19 @@
+package com.sunchaser.shushan.mojian.log.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * log ignore
+ *
+ * @author sunchaser admin@lilu.org.cn
+ * @since JDK8 2022/11/30
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LogIgnore {
+}
