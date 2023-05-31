8faa5ec290d4c8b3aa0b307bec25c537e0a042f5
llnancy <admin@lilu.org.cn>
2023-05-31 22:26:38 +0800

    feat: update input from file to stream

--- mojian-base/src/main/java/io/github/llnancy/mojian/base/util/ImageUtils.java
+++ mojian-base/src/main/java/io/github/llnancy/mojian/base/util/ImageUtils.java
@@ -1,11 +1,10 @@
 package io.github.llnancy.mojian.base.util;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.springframework.util.InvalidMimeTypeException;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
 
 /**
  * image util
@@ -18,30 +17,26 @@ public class ImageUtils {
     private ImageUtils() {
     }
 
-    public static ImmutablePair<Integer, Integer> getImageHeightAndWidth(MultipartFile file) throws Exception {
-        BufferedImage bufferedImage = doGetBufferedImage(file);
+    public static ImmutablePair<Integer, Integer> getImageHeightAndWidth(InputStream is) throws Exception {
+        BufferedImage bufferedImage = doGetBufferedImage(is);
         return ImmutablePair.of(bufferedImage.getHeight(), bufferedImage.getWidth());
     }
 
-    public static Integer getImageHeight(MultipartFile file) throws Exception {
-        return doGetBufferedImage(file).getHeight();
+    public static Integer getImageHeight(InputStream is) throws Exception {
+        return doGetBufferedImage(is).getHeight();
     }
 
-    public static Integer getImageWidth(MultipartFile file) throws Exception {
-        return doGetBufferedImage(file).getWidth();
+    public static Integer getImageWidth(InputStream is) throws Exception {
+        return doGetBufferedImage(is).getWidth();
     }
 
-    private static BufferedImage doGetBufferedImage(MultipartFile file) throws Exception {
-        if (file == null) {
-            throw new NullPointerException("file == null!");
+    private static BufferedImage doGetBufferedImage(InputStream is) throws Exception {
+        if (is == null) {
+            throw new NullPointerException("InputStream == null!");
         }
-        BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+        BufferedImage bufferedImage = ImageIO.read(is);
         if (bufferedImage == null) {
-            String contentType = file.getContentType();
-            if (contentType == null) {
-                throw new NullPointerException("contentType == null!");
-            }
-            throw new InvalidMimeTypeException(contentType, "文件非图片格式");
+            throw new IllegalArgumentException("文件非图片格式");
         }
         return bufferedImage;
     }
