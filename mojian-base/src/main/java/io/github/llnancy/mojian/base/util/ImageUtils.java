2588e623a278ce9d15aa983dcff1392728e867a0
llnancy <admin@lilu.org.cn>
2024-03-12 10:36:59 +0800

    feat: change param from MultipartFile to File

--- mojian-base/src/main/java/io/github/llnancy/mojian/base/util/ImageUtils.java
+++ mojian-base/src/main/java/io/github/llnancy/mojian/base/util/ImageUtils.java
@@ -1,10 +1,10 @@
 package io.github.llnancy.mojian.base.util;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * image util
@@ -17,24 +17,24 @@ public class ImageUtils {
     private ImageUtils() {
     }
 
-    public static ImmutablePair<Integer, Integer> getImageHeightAndWidth(MultipartFile file) throws Exception {
+    public static ImmutablePair<Integer, Integer> getImageHeightAndWidth(File file) throws Exception {
         BufferedImage bufferedImage = doGetBufferedImage(file);
         return ImmutablePair.of(bufferedImage.getHeight(), bufferedImage.getWidth());
     }
 
-    public static Integer getImageHeight(MultipartFile file) throws Exception {
+    public static Integer getImageHeight(File file) throws Exception {
         return doGetBufferedImage(file).getHeight();
     }
 
-    public static Integer getImageWidth(MultipartFile file) throws Exception {
+    public static Integer getImageWidth(File file) throws Exception {
         return doGetBufferedImage(file).getWidth();
     }
 
-    private static BufferedImage doGetBufferedImage(MultipartFile file) throws Exception {
+    private static BufferedImage doGetBufferedImage(File file) throws Exception {
         if (file == null) {
             throw new NullPointerException("file == null!");
         }
-        BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+        BufferedImage bufferedImage = ImageIO.read(file);
         if (bufferedImage == null) {
             throw new IllegalStateException("文件非图片格式");
         }
